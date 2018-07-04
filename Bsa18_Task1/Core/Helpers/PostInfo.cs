ff1fa3ec1403326190d9d23d6c468a9372cbbebf
LazloSoot <free2play1911@gmail.com>
2018-07-04 22:22:23 +0300

    helper classes added

---
+++ Bsa18_Task1/Core/Helpers/PostInfo.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+namespace Core.Helpers
+{
+    public struct PostInfo
+    {
+        public Post Post { get; set; }
+
+        public Comment LongestComment { get; set; }
+
+        public Comment BestComment { get; set; }
+
+        public int CommentsCount { get; set; }
+    }
+}
