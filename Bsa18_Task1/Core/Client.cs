56ee42a5ff06710f72f1261aef14ceddc0264405
LazloSoot <free2play1911@gmail.com>
2018-07-04 13:43:58 +0300

    Users initialization partially implemented

--- Bsa18_Task1/Core/Client.cs
+++ Bsa18_Task1/Core/Client.cs
@@ -5,14 +5,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using Core.Types;
 
 namespace Core
 {
     public class Client
     {
-        public Client()
+        private List<User> users;
+        //private List<Adress> adresses;
+        private static readonly Lazy<Client> instance;
+        private static readonly Uri baseUri;
+
+        public static Client Instance => instance.Value;
+
+        static Client()
         {
+            instance = new Lazy<Client>(() => new Client());
+            baseUri = new Uri(@"https://5b128555d50a5c0014ef1204.mockapi.io/");
+        }
+
+        private Client()
+        {
+            InitializeUsers();
+        }
+
+        public void InitializeUsers()
+        {
+            IEnumerable<User> usersTemp = LoadData<User>(Endpoint.users);
+            IEnumerable<Post> postsTemp = LoadData<Post>(Endpoint.posts);
+            IEnumerable<Comment> commentsTemp = LoadData<Comment>(Endpoint.comments);
+            IEnumerable<Todo> todosTemp = LoadData<Todo>(Endpoint.todos);
+
+            users = usersTemp.GroupJoin(postsTemp, user => user.Id, post => post.UserId,
+                (user, posts) => new User()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    CreatedAt = user.CreatedAt,
+                    Avatar = user.Avatar,
+                    Posts = posts.GroupJoin(commentsTemp, post => post.Id, comment => comment.PostId,
+                                (post, comments) => new Post()
+                                {
+                                    Id = post.Id,
+                                    Likes = post.Likes,
+                                    Title = post.Title,
+                                    Body = post.Body,
+                                    CreatedAt = post.CreatedAt,
+                                    UserId = post.UserId,
+                                    Comments = comments
+                                                //.Where(c => c.UserId == user.Id)
+                                                .ToList()
+                                })
+                                .ToList()
+                }
+            ).GroupJoin(todosTemp, user => user.Id, todo => todo.UserId,
+                (user, todos) => new User()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    CreatedAt = user.CreatedAt,
+                    Avatar = user.Avatar,
+                    Posts = user.Posts,
+                    Todos = todos.ToList()
+                }
+                ).ToList();
+        }
+
+        IEnumerable<T> LoadData<T>(Endpoint endpoint) where T : class, new()
+        {
+            var jsonData = string.Empty;
+            using (var wClient = new WebClient())
+            {
                 
+                        try
+                        {
+                            jsonData = wClient.DownloadString(new Uri(baseUri, endpoint.ToString()));
+                            if (String.IsNullOrEmpty(jsonData))
+                                throw new ArgumentNullException("");
+
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+            }
+
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonData) ?? null;
         }
 
         /// <summary>
@@ -22,6 +104,23 @@ namespace Core
         /// <returns></returns>
         public Dictionary<Post, int> GetCommentsCount(int userId)
         {
+            var a = users.Select(user => user.Posts
+                                                .Where(post => post.UserId == user.Id)
+                                                .Select(post => new
+                                                {
+                                                    Post = post,
+                                                    CommentsCount = post.Comments.Count()
+                                                }));
+
+
+            var b = users.FirstOrDefault(user => user.Id == userId)
+                                                .Posts
+                                                .Select(post => new
+                                                {
+                                                    Post = post,
+                                                    CommentsCount = post.Comments.Count()
+                                                });
+
             return null;
         }
 
@@ -32,6 +131,10 @@ namespace Core
         /// <returns></returns>
         public IEnumerable<Comment> GetCommentsList(int userId)
         {
+            var a = users.FirstOrDefault(user => user.Id == userId)
+                                    .Posts
+                                    .Select(post => post.Comments
+                                                        .Where(comment => comment.Body.Length < 50));
             return null;
         }
 
@@ -42,6 +145,13 @@ namespace Core
         /// <returns></returns>
         public Dictionary<int, string> GetTodos(int userId)
         {
+            var a = users.FirstOrDefault(user => user.Id == userId)
+                ?.Todos.Select(todo => new
+                {
+                    Id = todo.Id,
+                    Name = todo.Name
+                });
+
             return null;
         }
 
@@ -52,6 +162,7 @@ namespace Core
         /// <returns></returns>
         public IEnumerable<User> GetUserList()
         {
+           // var a = users.Select(user => user.Todos.OrderBy(todo => todo.Name.Max()))
             return null;
         }
 
@@ -70,6 +181,7 @@ namespace Core
         //Самый популярный пост пользователя(там где больше всего лайков)
         public object GetUserInfo(int userId)
         {
+            //return users.Select(user => user.)
             return null;
         }
 
