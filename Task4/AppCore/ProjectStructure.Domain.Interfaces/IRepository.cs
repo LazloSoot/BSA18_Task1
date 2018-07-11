b0ea8f93dadb0b49a211c5348e1e18ecec357c40
LazloSoot <free2play1911@gmail.com>
2018-07-12 01:05:07 +0300

    Domain services layer classes added

---
+++ Task4/AppCore/ProjectStructure.Domain.Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectStructure.Domain;
+
+namespace ProjectStructure.Domain.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        IEnumerable<T> GetAll();
+
+        T Get(long id);
+
+        void Insert(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Delete(long id);
+
+        void Remove(T entity);
+    }
+}
---
+++ Task4/AppCore/ProjectStructure.Domain.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace ProjectStructure.Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<T> Set<T>() where T : Entity;
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+    }
+}
