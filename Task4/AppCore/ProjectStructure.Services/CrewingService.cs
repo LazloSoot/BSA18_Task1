a90c9372fa6334da99708e6783b9887ac7403f10
LazloSoot <free2play1911@gmail.com>
2018-07-12 01:06:22 +0300

    Application services layer class added

---
+++ Task4/AppCore/ProjectStructure.Services/CrewingService.cs
@@ -0,0 +1,24 @@
+﻿using ProjectStructure.Domain;
+using ProjectStructure.Domain.Interfaces;
+using ProjectStructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectStructure.Services
+{
+    public class CrewingService : ICrewingService
+    {
+#warning заменить на ICrewRepository
+        private readonly IRepository<Crew> repository;
+
+        public CrewingService(IRepository<Crew> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void CreateCrew(Pilot pilot, IEnumerable<Stewardess> stewardesses)
+        {
+            repository.Insert(new Crew());
+        }
+    }
+}
