4f22340be9cf275bc58fc8cf69d96d5b249dd31f
LazloSoot <free2play1911@gmail.com>
2018-07-13 02:32:09 +0300

    Airport abs class (facade template) for coordinate airport services added

---
+++ Task4/AppCore/ProjectStructure.Services.Interfaces/Airport.cs
@@ -0,0 +1,27 @@
+﻿using ProjectStructure.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectStructure.Services.Interfaces
+{
+    public abstract class Airport
+    {
+        public IAircraftService AircraftService { get; }
+        public ICrewingService CrewingService { get; }
+        public IFlightOperationsService FlightOperationsService { get; }
+
+        public Airport(IAircraftService aircraftService, ICrewingService crewingService,
+            IFlightOperationsService flightOperationsService)
+        {
+            AircraftService = aircraftService;
+            CrewingService = crewingService;
+            FlightOperationsService = flightOperationsService;
+        }
+
+#warning Будет использовать так раз DTO !!!!!!!!11111111111
+        public abstract IEnumerable<Ticket> CreateFlight(Flight flightInfo, Departure departureInfo);
+
+        public abstract IEnumerable<Ticket> CreateFlight(long flightId, long departureId);
+    }
+}
