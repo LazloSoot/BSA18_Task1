c7fd09aaa702f18bb74aa6a65cab8a6285cca63a
LazloSoot <free2play1911@gmail.com>
2018-07-13 23:33:18 +0300

    -

--- Task4/AppCore/ProjectStructure.Services.Interfaces/Airport.cs
+++ Task4/AppCore/ProjectStructure.Services.Interfaces/Airport.cs
@@ -7,9 +7,9 @@ namespace ProjectStructure.Services.Interfaces
 {
     public abstract class Airport
     {
-        public IAircraftService AircraftService { get; }
-        public ICrewingService CrewingService { get; }
-        public IFlightOperationsService FlightOperationsService { get; }
+        protected IAircraftService AircraftService { get; }
+        protected ICrewingService CrewingService { get; }
+        protected IFlightOperationsService FlightOperationsService { get; }
 
         public Airport(IAircraftService aircraftService, ICrewingService crewingService,
             IFlightOperationsService flightOperationsService)
@@ -18,8 +18,7 @@ namespace ProjectStructure.Services.Interfaces
             CrewingService = crewingService;
             FlightOperationsService = flightOperationsService;
         }
-
-#warning Будет использовать так раз DTO !!!!!!!!11111111111
+        
         public abstract IEnumerable<Ticket> CreateFlight(Flight flightInfo, Departure departureInfo);
 
         public abstract IEnumerable<Ticket> CreateFlight(long flightId, long departureId);
