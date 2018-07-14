0f7922ae583784db5800c1e6d58448032911b1c6
LazloSoot <free2play1911@gmail.com>
2018-07-14 23:53:08 +0300

    migrations etc. added

---
+++ Task5/ExternalLayer/Tests/ConsoleTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ProjectStructure.Databases.MSSQL;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MSSQLContext context = new MSSQLContext();
+
+            context.Tickets.Add(new ProjectStructure.Domain.Ticket()
+            {
+                Price = 1000
+            });
+            Console.ReadLine();
+        }
+    }
+}
