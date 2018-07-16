f44bbe2d6f4a4cb2562c3e97510d0b65f897a646
LazloSoot <free2play1911@gmail.com>
2018-07-16 23:48:37 +0300

    getting tech condition and maintaining planes implemented

--- Task6/ExternalLayer/Infrastructure/ProjectStructure.Infrastructure.BL/AircraftService.cs
+++ Task6/ExternalLayer/Infrastructure/ProjectStructure.Infrastructure.BL/AircraftService.cs
@@ -5,6 +5,8 @@ using ProjectStructure.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Diagnostics;
 
 namespace ProjectStructure.Infrastructure.BL
 {
@@ -17,14 +19,70 @@ namespace ProjectStructure.Infrastructure.BL
         }
         #region Planes Methods
 
-        public TechCondition GetPlaneTechCondition(long id)
+        public CheckNeeded GetPlaneTechCondition(long id)
         {
-            throw new NotImplementedException();
+            var plane = uow.Planes.Get(id);
+            if (plane == null)
+                throw new ArgumentException($"Plane with id = {id} not found!");
+
+            return GetPlaneTechCondition(plane);
         }
 
-        public Plane CarryOutMaintenance(long id)
+        public CheckNeeded GetPlaneTechCondition(Plane plane)
         {
-            throw new NotImplementedException();
+            if (plane == null)
+                throw new ArgumentNullException("Plane is null");
+
+            // каждые 12 лет
+            if (DateTime.Now - plane.LastHeavyMaintenance >= TimeSpan.FromDays(4380))
+                return CheckNeeded.D_Check;
+            else if (plane.FlightHours >= 7500)
+                return CheckNeeded.C_Check;
+            else if (plane.FlightHours >= 3000)
+                return CheckNeeded.B_Check;
+            else if (plane.FlightHours >= 500)
+                return CheckNeeded.A_Check;
+            else
+                return CheckNeeded.None;
+        }
+
+        public Plane CarryOutMaintenance(Plane plane, CheckNeeded checks)
+        {
+            if (plane == null)
+                throw new ArgumentNullException("Plane is null");
+            if (checks == CheckNeeded.None)
+                return plane;
+
+            switch (checks)
+            {
+                case CheckNeeded.A_Check:
+                    Debug.WriteLine("A-check started.");
+                    Thread.Sleep(750);
+                    Debug.WriteLine("A-check successfuly completed.");
+                    break;
+                case CheckNeeded.B_Check:
+                    Debug.WriteLine("B-check started.");
+                    Thread.Sleep(1250);
+                    Debug.WriteLine("B-check successfuly completed.");
+                    break;
+                case CheckNeeded.C_Check:
+                    Debug.WriteLine("C-check started.");
+                    Thread.Sleep(2000);
+                    plane.FlightHours = 0;
+                    Debug.WriteLine("C-check successfuly completed.");
+                    break;
+                case CheckNeeded.D_Check:
+                    Debug.WriteLine("D-check started.");
+                    Thread.Sleep(4000);
+                    plane.LastHeavyMaintenance = DateTime.Now;
+                    plane.FlightHours = 0;
+                    Debug.WriteLine("D-check successfuly completed.");
+                    break;
+                default:
+                    break;
+            }
+
+            return plane;
         }
 
         public Plane AddPlane(Plane plane)
@@ -59,7 +117,7 @@ namespace ProjectStructure.Infrastructure.BL
             }
         }
 
-        public bool TryDeletePlane(int id)
+        public bool TryDeletePlane(long id)
         {
             if(uow.Planes.Delete(id))
             {
@@ -88,7 +146,7 @@ namespace ProjectStructure.Infrastructure.BL
             return uow.PlaneTypes.GetAll() ?? null;
         }
 
-        public PlaneType GetPlaneTypeInfo(int id)
+        public PlaneType GetPlaneTypeInfo(long id)
         {
             return uow.PlaneTypes.Get(id) ?? null;
         }
@@ -105,7 +163,7 @@ namespace ProjectStructure.Infrastructure.BL
             }
         }
 
-        public bool TryDeletePlaneType(int id)
+        public bool TryDeletePlaneType(long id)
         {
             if (uow.PlaneTypes.Delete(id))
             {
