b84e1a96abef8bf652f5596fa9e209cfdee77009
LazloSoot <free2play1911@gmail.com>
2018-07-16 23:44:27 +0300

    foreign keys ids added

--- Task6/AppCore/ProjectStructure.Domain/Departure.cs
+++ Task6/AppCore/ProjectStructure.Domain/Departure.cs
@@ -5,9 +5,12 @@ namespace ProjectStructure.Domain
     public class Departure : Entity
     {
         public Flight Flight { get; set; }
+        public long FlightId { get; set; }
         public DateTime DepartureTime { get; set; }
         public Crew Crew { get; set; }
+        public long CrewId { get; set; }
         public Plane Plane { get; set; }
+        public long PlaneId { get; set; }
 
         public Departure()
         {
