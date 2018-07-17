3c7f4e55dee954d90661f871e88473de8bd96d64
LazloSoot <free2play1911@gmail.com>
2018-07-17 23:20:00 +0300

    test proj. refferences added

---
+++ Task6/ExternalLayer/Tests/ProjectStructure.Tests/Api/Aircraft/CrewApiTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using ProjectStructure.WebApi.Controllers;
+
+namespace ProjectStructure.Tests.Api.Aircraft
+{
+    class CrewApiTest
+    {
+    }
+}
