b80c19107d9f85aa7cbf52d856df827d88dd269a
LazloSoot <free2play1911@gmail.com>
2018-07-18 17:44:41 +0300

    async methods implemented (beside methods with including props)

--- Task7/ExternalLayer/Infrastructure/ProjectStructure.Infrastructure.BL/CrewingService.cs
+++ Task7/ExternalLayer/Infrastructure/ProjectStructure.Infrastructure.BL/CrewingService.cs
@@ -3,21 +3,71 @@ using ProjectStructure.Domain.Interfaces;
 using ProjectStructure.Services.Interfaces;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.Threading;
+using Newtonsoft.Json;
 
 namespace ProjectStructure.Infrastructure.BL
 {
     public class CrewingService : ICrewingService
     {
         private readonly IDbCrewingUnitOfWork uow;
+        private static SemaphoreSlim semaphore = new SemaphoreSlim(1);
         public CrewingService(IDbCrewingUnitOfWork crewingUnitOfWork)
         {
             uow = crewingUnitOfWork;
         }
+
         #region Crews
+
+        public async Task LoadOutSourceCrewsAsync(string uri, CancellationToken ct)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                if (string.IsNullOrEmpty(uri))
+                    throw new ArgumentNullException("Uri is null!");
+
+                var jsonData = string.Empty;
+                using (var wClient = new WebClient())
+                {
+
+                    try
+                    {
+                        jsonData = await wClient.DownloadStringTaskAsync(uri);
+                        if (String.IsNullOrEmpty(jsonData))
+                            throw new ArgumentNullException("Failed to load string data!");
+
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                var crews = await Task.Run(() => JsonConvert.DeserializeObject<IEnumerable<Crew>>(jsonData) ?? null);
+
+                if (crews == null)
+                    throw new ArgumentNullException("Failed to deserialize data!");
+
+                await uow.Crews.InsertRangeAsync(crews, ct);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+        
 
         public Crew GetCrewInfo(long id)
         {
             return uow.Crews.Get(id) ?? null;
+        }
+
+        public async Task<Crew> GetCrewInfoAsync(long id, CancellationToken ct)
+        {
+            return await uow.Crews.GetAsync(id, ct);
         }
 
         public Crew GetIncludedCrewInfo(long id, bool isCatched = false)
@@ -31,6 +81,11 @@ namespace ProjectStructure.Infrastructure.BL
         public IEnumerable<Crew> GetAllCrewsInfo()
         {
             return uow.Crews.GetAll() ?? null;
+        }
+
+        public async Task<IEnumerable<Crew>> GetAllCrewsInfoAsync(CancellationToken ct = default(CancellationToken))
+        {
+            return await uow.Crews.GetAllAsync(ct) ?? null;
         }
 
         public Crew CreateCrew(long pilotId, IEnumerable<long> stewardessesIds)
@@ -47,6 +102,23 @@ namespace ProjectStructure.Infrastructure.BL
                 return null;
             else
                 uow.SaveChanges();
+            return item;
+        }
+
+        public async Task<Crew> CreateCrewAsync(long pilotId, IEnumerable<long> stewardessesIds, CancellationToken ct = default(CancellationToken))
+        {
+            var pilot = await uow.Pilots.GetAsync(pilotId, ct);
+            if (pilot == null)
+                return null;
+            var stewardesses = uow.Stewardesses.FindBy(s => stewardessesIds.Contains(s.Id));
+            if (stewardesses == null || stewardesses.Count() < 2)
+                return null;
+
+            var item = uow.Crews.Update(new Crew(pilot, stewardesses.ToList()));
+            if (item == null)
+                return null;
+            else
+                await uow.SaveChangesAsync(ct);
             return item;
         }
 
@@ -79,6 +151,35 @@ namespace ProjectStructure.Infrastructure.BL
             }
         }
 
+        public async Task<Crew> ReformCrewAsync(long crewId, long pilotId, IEnumerable<long> stewardessesIds, CancellationToken ct = default(CancellationToken))
+        {
+            var crew = await uow.Crews.GetAsync(crewId, ct);
+            if (crew == null)
+                return null;
+
+            if (crew.Pilot.Id != pilotId)
+            {
+                var pilot = await uow.Pilots.GetAsync(pilotId, ct);
+                if (pilot == null)
+                    return null;
+                crew.Pilot = pilot;
+            }
+
+            var stewardesses = uow.Stewardesses.FindBy(s => stewardessesIds.Contains(s.Id));
+            if (stewardesses == null || stewardesses.Count() < 2)
+                return null;
+            crew.Stewardesses = stewardesses.ToList();
+
+            var item = uow.Crews.Update(crew);
+            if (item == null)
+                return null;
+            else
+            {
+                await uow.SaveChangesAsync(ct);
+                return item;
+            }
+        }
+
         public bool TryDeleteCrew(long id)
         {
             if (uow.Crews.Delete(id))
@@ -88,18 +189,39 @@ namespace ProjectStructure.Infrastructure.BL
             }
             return false;
         }
+
+        public async Task<bool> TryDeleteCrewAsync(long id, CancellationToken ct = default(CancellationToken))
+        {
+            if (uow.Crews.Delete(id))
+            {
+                await uow.SaveChangesAsync(ct);
+                return true;
+            }
+            return false;
+        }
+
         #endregion
 
         #region Pilots
-
+        
         public Pilot GetPilotInfo(long id)
         {
             return uow.Pilots.Get(id) ?? null;
         }
 
+        public async Task<Pilot> GetPilotInfoAsync(long id, CancellationToken ct = default(CancellationToken))
+        {
+            return await uow.Pilots.GetAsync(id, ct) ?? null;
+        }
+
         public IEnumerable<Pilot> GetAllPilotsInfo()
         {
             return uow.Pilots.GetAll() ?? null;
+        }
+
+        public async Task<IEnumerable<Pilot>> GetAllPilotsInfoAsync(CancellationToken ct = default(CancellationToken))
+        {
+            return await uow.Pilots.GetAllAsync(ct);
         }
 
         public Pilot HirePilot(Pilot pilot)
@@ -112,11 +234,31 @@ namespace ProjectStructure.Infrastructure.BL
             return item;
         }
 
+        public async Task<Pilot> HirePilotAsync(Pilot pilot, CancellationToken ct = default(CancellationToken))
+        {
+            var item = await uow.Pilots.InsertAsync(pilot, ct);
+            if (item == null)
+                return null;
+            else
+                await uow.SaveChangesAsync(ct);
+            return item;
+        }
+
         public bool TryDismissPilot(long id)
         {
             if (uow.Pilots.Delete(id))
             {
                 uow.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> TryDismissPilotAsync(long id, CancellationToken ct = default(CancellationToken))
+        {
+            if (uow.Pilots.Delete(id))
+            {
+                await uow.SaveChangesAsync(ct);
                 return true;
             }
             return false;
@@ -135,6 +277,19 @@ namespace ProjectStructure.Infrastructure.BL
             }
         }
 
+        public async Task<Pilot> UpdatePilotInfoAsync(long id, Pilot pilot, CancellationToken ct = default(CancellationToken))
+        {
+            pilot.Id = id;
+            var item = uow.Pilots.Update(pilot);
+            if (item == null)
+                return null;
+            else
+            {
+                await uow.SaveChangesAsync(ct);
+                return item;
+            }
+        }
+
         #endregion
 
         #region Stewardesses
@@ -143,10 +298,20 @@ namespace ProjectStructure.Infrastructure.BL
         {
             return uow.Stewardesses.Get(id) ?? null;
         }
+        
+        public async Task<Stewardess> GetStewardessInfoAsync(long id, CancellationToken ct = default(CancellationToken))
+        {
+            return await uow.Stewardesses.GetAsync(id, ct) ?? null;
+        }
 
         public IEnumerable<Stewardess> GetAllStewardessesInfo()
         {
             return uow.Stewardesses.GetAll() ?? null;
+        }
+
+        public async Task<IEnumerable<Stewardess>> GetAllStewardessesInfoAsync(CancellationToken ct = default(CancellationToken))
+        {
+            return await uow.Stewardesses.GetAllAsync(ct) ?? null;
         }
 
         public Stewardess HireStewardess(Stewardess stewardess)
@@ -156,6 +321,16 @@ namespace ProjectStructure.Infrastructure.BL
                 return null;
             else
                 uow.SaveChanges();
+            return item;
+        }
+
+        public async Task<Stewardess> HireStewardessAsync(Stewardess stewardess, CancellationToken ct = default(CancellationToken))
+        {
+            var item = await uow.Stewardesses.InsertAsync(stewardess, ct);
+            if (item == null)
+                return null;
+            else
+                await uow.SaveChangesAsync(ct);
             return item;
         }
 
@@ -172,11 +347,34 @@ namespace ProjectStructure.Infrastructure.BL
             }
         }
 
+        public async Task<Stewardess> UpdateStewardessInfoAsync(long id, Stewardess stewardess, CancellationToken ct = default(CancellationToken))
+        {
+            stewardess.Id = id;
+            var item = uow.Stewardesses.Update(stewardess);
+            if (item == null)
+                return null;
+            else
+            {
+                await uow.SaveChangesAsync(ct);
+                return item;
+            }
+        }
+
         public bool TryDismissStewardess(long id)
         {
             if (uow.Stewardesses.Delete(id))
             {
                 uow.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> TryDismissStewardessAsync(long id, CancellationToken ct = default(CancellationToken))
+        {
+            if (uow.Stewardesses.Delete(id))
+            {
+                await uow.SaveChangesAsync(ct);
                 return true;
             }
             return false;
