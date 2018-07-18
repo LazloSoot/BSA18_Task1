97dd1c168c86f1bee08d43b45bfc9454403cf511
LazloSoot <free2play1911@gmail.com>
2018-07-18 23:49:07 +0300

    flighs with delay extension added

---
+++ Task7/ExternalLayer/Infrastructure/ProjectStructure.Infrastructure.Shared/Helpers/FlightOperationsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ProjectStructure.Domain;
+using ProjectStructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ProjectStructure.Infrastructure.Shared.Helpers
+{
+    public static class FlightOperationsExtensions
+    {
+        public static async Task<IEnumerable<Flight>> GetFlightsWithDelay(this IFlightOperationsService service)
+        {
+            TaskCompletionSource<IEnumerable<Flight>> tcs = new TaskCompletionSource<IEnumerable<Flight>>();
+            Timer t = new Timer(1500);
+            t.Elapsed += (s, e) =>
+           {
+
+           };
+
+        }
+        
+    }
+}
