5c3b8571d0d544b5e5b89e7177f671f011818abd
LazloSoot <free2play1911@gmail.com>
2018-07-18 17:34:56 +0300

    async generic methods added

--- Task7/AppCore/ProjectStructure.Domain.Interfaces/Repo/IDbRepository.cs
+++ Task7/AppCore/ProjectStructure.Domain.Interfaces/Repo/IDbRepository.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ProjectStructure.Domain.Interfaces
 {
     public interface IDbRepository<T> : IRepository<T> where T : Entity
     {
+
+        Task<T> GetAsync(long id, CancellationToken ct);
+
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken ct);
+
+        Task<T> InsertAsync(T entity, CancellationToken ct);
+
+        Task InsertRangeAsync(IEnumerable<T> entities, CancellationToken ct);
+
         IEnumerable<T> GetAllIncluding(bool isCached = false, params Expression<Func<T, object>>[] includeProperties);
 
         IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate, bool isCached = false);
