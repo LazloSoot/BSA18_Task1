e922aea9ced63b6a14b3caf19edb4d7fd90ea4e1
LazloSoot <free2play1911@gmail.com>
2018-08-01 06:16:36 +0300

    dateTime picker added

---
+++ Task11/AirportUI/ValueConverters/DateTimeToDateTimeOffsetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace AirportUI.ValueConverters
+{
+    public class DateTimeToDateTimeOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new DateTimeOffset((DateTime)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            DateTimeOffset sourceTime = (DateTimeOffset)value;
+            return sourceTime.DateTime;
+        }
+    }
+}
